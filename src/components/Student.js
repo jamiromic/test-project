4c0572d64034d6d1b4ee78bf4eeaf1520d2871a9
jamiromic <jamiromic@gmail.com>
2023-03-05 23:09:37 +0100

    add details students

---
+++ src/components/Student.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
+function StudentDetail() {
+  // Ottengo l'ID dello studente dalla URL utilizzando useParams()
+  const { id } = useParams();
+
+  // Crea uno stato per contenere le informazioni sullo studente
+  const [student, setStudent] = useState(null);
+
+  // Recupera le informazioni sullo studente dalle API utilizzando l'ID
+  useEffect(() => {
+    fetch(`http://localhost:8080/api/students/${id}`)
+      .then(response => response.json())
+      .then(data => setStudent(data))
+      .catch(error => console.log(error));
+  }, [id]);
+
+  // Mostro un messaggio di caricamento se le informazioni dello studente non sono ancora state recuperate
+  if (!student) {
+    return <p>Caricamento in corso...</p>;
+  }
+
+  // Mostro le informazioni sullo studente
+  return (
+    <div>
+      <h1>{student.name}</h1>
+      <p>Età: {student.age}</p>
+      <p>Voto: {student.grade}</p>
+      <Link to="/students">Torna alla lista degli studenti</Link>
+    </div>
+  );
+}
+
+export default StudentDetail;
---
+++ src/components/StudentList.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/StudentsList.css';
+
+
+
+function StudentList() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/students/')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.log(error));
+  });
+
+
+  function handleClick(event) {
+    const idItem = event.target.id;
+    fetch('http://localhost:8080/api/students/delete/' + idItem, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }
+
+  return (
+    
+    <div>
+      <h1 className='title'>List Students:</h1>
+      <div className='students_wrapper'>
+        {data.map(item => (
+          <div className='card_wrapper' key={item.id}>
+            <Link to={`/students/${item.id}`} />
+            <a href={`students/${item.id}`}>
+              <ul>
+                <li>Nome : {item.name}</li>
+                <li>Età : {item.age}</li>
+                <li>Voto : {item.grade}</li>
+              </ul>
+            </a>
+          <button id={item.id} onClick={handleClick}>Elimina dati</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default StudentList;
